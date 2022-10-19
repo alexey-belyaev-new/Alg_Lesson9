ec9b870569bca413c9c307be3450978b8a668db1
alexey-belyaev-new <78504218+alexey-belyaev-new@users.noreply.github.com>
2022-10-19 19:37:37 +0300

    Add files via upload

---
+++ Alg_Lesson9.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int qsize = 100;
+
+char Queue[qsize];
+int start = 0;
+int last = -1;
+
+bool enqueue(char data) {
+    if (last < qsize) {
+        Queue[++last] = data;
+        return true;
+    }
+    else {
+        cout << "Queue is full" << endl;
+        return false;
+    }
+}
+char dequeue() {
+    if (start <= last) {
+        return Queue[start++];
+    }
+    else {
+        cout << "Queue is empty";
+        return -1;
+    }
+}
+
+
+int main()
+{
+    enqueue('a');
+    enqueue('b');
+    enqueue('c');
+    enqueue('d');
+    while (last >= start) {
+        cout << dequeue();
+    }
+}
+
